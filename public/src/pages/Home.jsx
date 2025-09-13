c9701080dfef39db3d2e4961d302abc383511f4b
SamyBK07 <kpeigounousamiou@gmail.com>
2025-09-13 14:10:23 +0100

    Update Home.jsx

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -1,1 +1,21 @@
+import { Link } from 'react-router-dom'
 
+function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h2>Bienvenue chez Saga Global</h2>
+      <p>Découvrez nos gammes de produits :</p>
+
+      <div style={{ display: "flex", justifyContent: "center", gap: "2rem", marginTop: "2rem" }}>
+        <Link to="/lithotherapie" style={{ padding: "1rem", border: "1px solid #000" }}>
+          Lithothérapie
+        </Link>
+        <Link to="/cosmetique" style={{ padding: "1rem", border: "1px solid #000" }}>
+          Cosmétique
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
