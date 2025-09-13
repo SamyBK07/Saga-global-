63845fbbb39817b8c81e8cabec8a3dbb486fef79
SamyBK07 <204034392+SamyBK07@users.noreply.github.com>
2025-09-13 20:42:26 +0100

    commit

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,21 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Categories from "./pages/Categories";
-import Lithotherapie from "./pages/Lithotherapie";
-import Cosmetique from "./pages/Cosmetique";
-import { CartProvider } from "./context/CartContext";
 
 export default function App() {
   return (
-    <CartProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/lithotherapie" element={<Lithotherapie />} />
-        <Route path="/cosmetique" element={<Cosmetique />} />
-      </Routes>
-    </CartProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* Plus tard : /categories, /lithotherapie, /cosmetique */}
+    </Routes>
   );
 }
