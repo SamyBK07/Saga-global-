8076bedc80e39826328c5c54a8a50096a162b516
SamyBK07 <kpeigounousamiou@gmail.com>
2025-09-13 16:39:35 +0100

    Update App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,20 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Lithotherapie from './pages/Lithotherapie'
-import Cosmetique from './pages/Cosmetique'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Lithotherapie from "./pages/Lithotherapie";
+import Cosmetique from "./pages/Cosmetique";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/lithotherapie" element={<Lithotherapie />} />
         <Route path="/cosmetique" element={<Cosmetique />} />
       </Routes>
-    </>
-  )
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
