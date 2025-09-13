38bc89a7cceaecd48b90cd19c4ecbdf8b6fc33fd
SamyBK07 <kpeigounousamiou@gmail.com>
2025-09-13 14:08:20 +0100

    Update App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,1 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Lithotherapie from './pages/Lithotherapie'
+import Cosmetique from './pages/Cosmetique'
 
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lithotherapie" element={<Lithotherapie />} />
+        <Route path="/cosmetique" element={<Cosmetique />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
