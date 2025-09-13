ab0e44ae659a7016bfac740df8e98cf09b30de70
SamyBK07 <204034392+SamyBK07@users.noreply.github.com>
2025-09-13 21:53:49 +0100

    commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Contactez-nous</h1>
+      <p>Téléphone : +229 12 34 56 78</p>
+      <p>Email : contact@sagaglobal.com</p>
+    </div>
+  );
+}
