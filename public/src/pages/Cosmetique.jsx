a7dedf48b6f135d8d3def604348c4d646f2e6e45
SamyBK07 <kpeigounousamiou@gmail.com>
2025-09-13 14:11:25 +0100

    Update Cosmetique.jsx

--- public/src/pages/Cosmetique.jsx
+++ public/src/pages/Cosmetique.jsx
@@ -1,1 +1,17 @@
+import products from '../data/products.json'
+import ProductCard from '../components/ProductCard'
 
+function Cosmetique() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Produits Cosmétiques</h2>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {products.cosmetique.map(p => (
+          <ProductCard key={p.id} product={p} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Cosmetique
