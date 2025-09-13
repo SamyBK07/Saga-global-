77e298298e786691a3358d43a363cd1fd9b4289d
SamyBK07 <kpeigounousamiou@gmail.com>
2025-09-13 16:35:08 +0100

    Create Footer.jsx

---
+++ public/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer style={{ backgroundColor: "black", color: "white", padding: "1rem", textAlign: "center" }}>
+      <p>📞 Contact : +229 00 00 00 00</p>
+      <p>📧 Email : contact@sagaglobal.com</p>
+    </footer>
+  );
+}
