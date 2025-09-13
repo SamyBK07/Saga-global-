018054fb03db43cf830ee6426fce854ff6a0686f
SamyBK07 <kpeigounousamiou@gmail.com>
2025-09-13 14:10:50 +0100

    Update Lithotherapie.jsx

--- public/src/pages/Lithotherapie.jsx
+++ public/src/pages/Lithotherapie.jsx
@@ -1,1 +1,17 @@
+import products from '../data/products.json'
+import ProductCard from '../components/ProductCard'
 
+function Lithotherapie() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Produits Lithothérapie</h2>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {products.lithotherapie.map(p => (
+          <ProductCard key={p.id} product={p} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Lithotherapie
