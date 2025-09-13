7cf43f9b23c12a7fd3d1a4d3a33cf30ea5ba06cc
SamyBK07 <kpeigounousamiou@gmail.com>
2025-09-13 16:44:32 +0100

    Update Navbar.jsx

--- public/src/components/Navbar.jsx
+++ public/src/components/Navbar.jsx
@@ -1,18 +1,31 @@
-import { Link } from 'react-router-dom'
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const { cart } = useContext(CartContext); // panier global
+
   return (
-    <nav style={{ padding: "1rem", background: "#f0f0f0" }}>
-      <h1 style={{ fontSize: "2rem", textAlign: "center", margin: 0 }}>
+    <nav style={{
+      padding: "1rem",
+      background: "black",
+      color: "white",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center"
+    }}>
+      <h1 style={{ fontSize: "2rem", color: "yellow", margin: 0 }}>
         Saga Global
       </h1>
-      <div style={{ textAlign: "center", marginTop: "0.5rem" }}>
-        <Link to="/" style={{ margin: "0 1rem" }}>Accueil</Link>
-        <Link to="/lithotherapie" style={{ margin: "0 1rem" }}>Lithothérapie</Link>
-        <Link to="/cosmetique" style={{ margin: "0 1rem" }}>Cosmétique</Link>
+
+      <div>
+        <Link to="/" style={{ color: "yellow", margin: "0 1rem" }}>Accueil</Link>
+        <Link to="/lithotherapie" style={{ color: "yellow", margin: "0 1rem" }}>Lithothérapie</Link>
+        <Link to="/cosmetique" style={{ color: "yellow", margin: "0 1rem" }}>Cosmétique</Link>
+        <span style={{ marginLeft: "1rem" }}>🛒 {cart.length}</span>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
