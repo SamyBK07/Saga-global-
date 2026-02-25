9dcb23a488f8272a9d741a18e1c25ca97c2119a3
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-25 02:51:48 +0100

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,1 +1,29 @@
+import { useCart } from "../context/CartContext";
+import CartItem from "../components/CartItem";
+import formatPrice from "../utils/formatPrice";
+import { Link } from "react-router-dom";
 
+const Cart = () => {
+  const { cartItems, getTotalPrice } = useCart();
+
+  if (cartItems.length === 0)
+    return <p>Votre panier est vide.</p>;
+
+  return (
+    <div>
+      <h2>Votre Panier</h2>
+
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+
+      <h3>Total : {formatPrice(getTotalPrice())}</h3>
+
+      <Link to="/checkout">
+        <button>Commander</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Cart;
