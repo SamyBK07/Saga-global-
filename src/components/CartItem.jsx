73be351b554f61150b2bf1329f2d4c6de3696ca1
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-25 02:45:00 +0100

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,33 @@
+import { useCart } from "../context/CartContext";
+import formatPrice from "../utils/formatPrice";
 
+const CartItem = ({ item }) => {
+  const { updateQuantity, removeFromCart } = useCart();
+
+  return (
+    <div className="cart-item">
+      <img
+        src={`/images/${item.category}/${item.image}`}
+        alt={item.name}
+        width="80"
+      />
+      <div>
+        <h4>{item.name}</h4>
+        <p>{formatPrice(item.price)}</p>
+        <input
+          type="number"
+          min="1"
+          value={item.quantity}
+          onChange={(e) =>
+            updateQuantity(item.id, Number(e.target.value))
+          }
+        />
+        <button onClick={() => removeFromCart(item.id)}>
+          Supprimer
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
