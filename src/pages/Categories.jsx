7fb428f699ab3ccdd9f575f06b54dcb6fd85e4eb
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-25 02:50:15 +0100

    Update Categories.jsx

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,18 +1,14 @@
 import { Link } from "react-router-dom";
-import "./Categories.css"; // CSS pour styliser les catégories
 
-export default function Categories() {
+const Categories = () => {
   return (
-    <div className="categories-container">
+    <div>
       <h2>Nos catégories</h2>
-      <div className="categories">
-        <Link to="/lithotherapie" className="category-card">
-          Lithothérapie
-        </Link>
-        <Link to="/cosmetique" className="category-card">
-          Cosmétique
-        </Link>
-      </div>
+      <Link to="/cosmetique">Cosmétique</Link>
+      <br />
+      <Link to="/lithotherapie">Lithothérapie</Link>
     </div>
   );
-}
+};
+
+export default Categories;
