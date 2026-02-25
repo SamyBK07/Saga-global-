0c9cb5440c64f9852eb4eb1a866d3df76cda449c
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-25 03:09:21 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,37 @@
 import products from "../data/products.json";
 import ProductCard from "../components/ProductCard";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Le bien-être à votre portée</h1>
-      <p>Cosmétiques de qualité • Paiement à la livraison</p>
+    <div className="home">
 
-      <h2>Nos indispensables beauté</h2>
+      {/* HERO */}
+      <section className="hero">
+        <div className="hero-content">
+          <h1>✨ Le bien-être à votre portée</h1>
+          <p>Cosmétiques de qualité • Paiement à la livraison</p>
 
-      <div className="products-grid">
-        {products.slice(0, 4).map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+          <Link to="/categories">
+            <button className="hero-btn">
+              Découvrir nos produits
+            </button>
+          </Link>
+        </div>
+      </section>
+
+      {/* PRODUITS PHARES */}
+      <section>
+        <h2 className="section-title">Nos indispensables beauté</h2>
+
+        <div className="products-grid">
+          {products.slice(0, 4).map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+
     </div>
   );
 };
