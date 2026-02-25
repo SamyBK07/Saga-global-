f4a6bae4cfe0f70c571e40657cb647a79faf6b5a
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-25 02:53:03 +0100

    Update Success.jsx

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,1 +1,10 @@
+const Success = () => {
+  return (
+    <div>
+      <h2>Votre commande a bien été reçue</h2>
+      <p>Nous vous contactons sous 30 minutes.</p>
+    </div>
+  );
+};
 
+export default Success;
