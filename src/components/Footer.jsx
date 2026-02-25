b3ec7797e9565439ad661ddd3f15326e2887cbd2
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-25 02:46:30 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,9 @@
-import { FaPhone, FaWhatsapp, FaFacebook, FaEnvelope } from "react-icons/fa";
-import "./Footer.css";
-
-function Footer() {
+const Footer = () => {
   return (
     <footer className="footer">
-      <p>Nous contacter</p>
-      <div className="contacts">
-        <a href="tel:+22912345678"><FaPhone /> Téléphone</a>
-        <a href="https://wa.me/22912345678"><FaWhatsapp /> WhatsApp</a>
-        <a href="https://facebook.com/tonpage"><FaFacebook /> Facebook</a>
-        <a href="mailto:contact@sagaglobal.com"><FaEnvelope /> Email</a>
-      </div>
+      <p>© 2026 Saga Globale</p>
     </footer>
   );
-}
+};
 
 export default Footer;
