6018c9629c1ae425ccd7c85889e96ff312bbe879
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-25 02:56:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Categories from "./pages/Categories";
+import Cosmetique from "./pages/Cosmetique";
+import Lithotherapie from "./pages/Lithotherapie";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Success from "./pages/Success";
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
+        <Route path="/cosmetique" element={<Cosmetique />} />
+        <Route path="/lithotherapie" element={<Lithotherapie />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/success" element={<Success />} />
