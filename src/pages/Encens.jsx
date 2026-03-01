8b0cf7a0c8ef04b8d39198f9571bc05d8b0f689f
SamyBK07 <kpeigounousamiou@gmail.com>
2026-03-01 15:28:23 +0100

    Update Encens.jsx

--- src/pages/Encens.jsx
+++ src/pages/Encens.jsx
@@ -1,1 +1,22 @@
+import products from "../data/products.json";
+import ProductCard from "../components/ProductCard";
 
+const Encens = () => {
+  const encensProducts = products.filter(
+    (product) => product.category === "encens"
+  );
+
+  return (
+    <div>
+      <h2>Encens</h2>
+
+      <div className="products-grid">
+        {encensProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Encens;
