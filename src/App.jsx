975d7fef94e1fe037815e07d439a5dd2d072dfbe
SamyBK07 <kpeigounousamiou@gmail.com>
2026-07-16 15:42:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import BottomNav from "./components/BottomNav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Categories from "./pages/Categories";
 import Cosmetique from "./pages/Cosmetique";
 import Lithotherapie from "./pages/Lithotherapie";
-import Encens from "./pages/Encens"; // 👈 AJOUT
+import Encens from "./pages/Encens";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Success from "./pages/Success";
@@ -13,18 +14,21 @@ import Success from "./pages/Success";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/cosmetique" element={<Cosmetique />} />
-        <Route path="/lithotherapie" element={<Lithotherapie />} />
-        <Route path="/encens" element={<Encens />} /> {/* 👈 AJOUT */}
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
+      <Header />
+      <main className="site-main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/cosmetique" element={<Cosmetique />} />
+          <Route path="/lithotherapie" element={<Lithotherapie />} />
+          <Route path="/encens" element={<Encens />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </main>
       <Footer />
+      <BottomNav />
     </BrowserRouter>
   );
 }
