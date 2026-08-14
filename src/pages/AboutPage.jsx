cff49d3b2d97b86f3f943a5d6a48ff7bbc3b7021
SamyBK07 <kpeigounousamiou@gmail.com>
2026-08-14 15:49:49 +0100

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,12 +9,12 @@ const AboutPage = () => {
       <div className="about-text">
         <h2>Georgette Azonnadou</h2>
         <p>
-          Créée en 2021 par Georgette Azonnadou, Saga Global est née d'une
+          Créée en 2021, Saga Global est née d'une
           passion pour la cosmétique.
         </p>
         <p>
           Elle s'est étendue aujourd'hui et couvre plusieurs autres domaines :
-          Thé, Savons, Encens.
+          Thé, Savons, Encens etc
         </p>
       </div>
     </div>
